5ae0a701ae4714ba573c00b0a0e03cfd8a032c84
faisal <faisal@gmail.com>
2025-07-29 02:05:35 +0500

    important-changes

--- src/Components/MobileScreen/MobileScreenNav.jsx
+++ src/Components/MobileScreen/MobileScreenNav.jsx
@@ -9,7 +9,7 @@ const MobileScreenNav = () => {
   const [showDropdown, setShowDropdown] = useState(false);
 
   return (
-    <div className='absolute w-full z-50 p-4 '>
+    <div className=' w-full z-50 p-4 bg-black/90'>
       {/* Top user icons */}
       <div className='flex items-center justify-between'>
         <img
@@ -17,7 +17,7 @@ const MobileScreenNav = () => {
           className="w-10 h-10 rounded-full border border-white/20 shadow-md"
           alt="User"
         />
-        <div className='flex items-center gap-3 text-white text-xl'>
+        <div className='flex items-center gap-3 text-white text-2xl'>
           <MdDownloadDone />
           <MdSearch />
         </div>
@@ -27,13 +27,13 @@ const MobileScreenNav = () => {
       <div className='flex items-center gap-3 mt-4' >
         <button
           
-          className="flex items-center justify-between w-40 bg-white/10 hover:bg-white/20 px-4 py-2 rounded-full text-white text-sm font-semibold transition"
+          className="flex items-center  gap-4  border hover:bg-white/20 px-4 py-1 rounded-full text-white text-sm font-semibold transition"
         >
-          All Categories <RiArrowDropDownLine className="text-2xl" />
+          TV Shows 
         </button>
           <button
             
-            className="flex items-center justify-between w-40 bg-white/10 hover:bg-white/20 px-4 py-2 rounded-full text-white text-sm font-semibold transition"
+            className="flex tems-center gap-4 border hover:bg-white/20 px-4 py-1 rounded-full text-white text-sm font-semibold transition"
           >
             All Categories <RiArrowDropDownLine className="text-2xl" />
           </button>
